013893529b64e8db6dd90d760f5ec1281798be7d
Ella Jameson <ellagjameson@gmail.com>
2019-12-21 14:03:03 -0700

    Update Vac_Former.ino

--- Vac_Former/Vac_Former.ino
+++ Vac_Former/Vac_Former.ino
@@ -1,5 +1,3 @@
-#include <Wire.h>
-
 //YWROBOT
 //Compatible with the Arduino IDE 1.0
 //Library version:1.1
@@ -7,15 +5,17 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include "Arduino.h"
-#include "PCF8574.h"
 
 LiquidCrystal_I2C lcd(0x3F,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
-PCF8574 pcf8574(0x20); // Set i2c address
-int buttonPin = 2; //The Pin your button is attached to
+
 float pressLength_milliSeconds = 0;//This value will be recorded in seconds
+
+uint8_t button_pin = 2; //The Pin your button is attached to. Can ONLY br 2 or 3 for interrupt to work!
+uint8_t relay_pins[] = {3, 4, 5, 6, 7, 8, 9, 10}; // The pins for relays 0-7
 
 void setup()
 {
+  Serial.begin(9600);
   
   lcd.init();                      // initialize the lcd 
   // Print a message to the LCD.
@@ -25,25 +25,97 @@ void setup()
   lcd.setCursor(0,1);
   lcd.print("  (0x3F,16,2)");
 
-    // Set pinMode to OUTPUT
-  pcf8574.pinMode(P0, OUTPUT);
-  pcf8574.pinMode(P1, OUTPUT);
-  pcf8574.pinMode(P2, OUTPUT);
-  pcf8574.pinMode(P3, OUTPUT);
-
-  //Record *roughly* the tenths of seconds the button in being held down
-  while (digitalRead(buttonPin) == LOW ){ 
-
-    delay(100);  //if you want more resolution, lower this number 
-    pressLength_milliSeconds = pressLength_milliSeconds + 100;   
-
-    //display how long button is has been held
-    
-    lcd.println(pressLength_milliSeconds);
-
-  }//close while
-
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  pinMode(button_pin, INPUT_PULLUP);   // Keep in mind, when pin 2 has ground voltage applied, we know the button is being pressed
   
+  // Set pinMode to OUTPUT
+  for(int i=0; i<8; i++)
+  {
+    pinMode(relay_pins[i], OUTPUT);
+  }
 
+  // Setup button interrupts
+  attachInterrupt(digitalPinToInterrupt(button_pin), 
+                  button_ISR,
+                  CHANGE);
+  
+  // Turn off all relays
+  set_relays(0,0,0,0,0,0,0,0);
+}
 
 void loop()
+{
+  delay(1000);
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+}
+
+void set_relay(uint8_t relay, bool pin_state)
+{
+  // Usage: set_relay(2, true);
+  digitalWrite(relay_pins[relay], !pin_state);
+}
+
+void set_relays(bool in0, bool in1, bool in2, bool in3,
+                bool in4, bool in5, bool in6, bool in7)
+{
+  // Usage: set_relays(0,1,1,0,0,0,1,1);
+  bool pin_states[] = {in0, in1, in2, in3, in4, in5, in6, in7};
+  for(int i=0; i<8; i++)
+  {
+    set_relay(i, pin_states[i]);
+  }
+}
+
+#define NUM_STATES 8
+int my_state = 0;
+void my_func()
+{
+  switch (my_state)
+  {
+    case 0:
+      set_relay(0, HIGH);
+      break;
+    case 1:
+      set_relay(1, HIGH);
+      break;
+    case 2:
+      set_relay(2, HIGH);
+      break;
+    case 3:
+      set_relay(3, HIGH);
+      break;
+    case 4:
+      set_relay(4, HIGH);
+      break;
+    case 5:
+      set_relay(5, HIGH);
+      break;
+    case 6:
+      set_relay(6, HIGH);
+      break;
+    case 7:
+      set_relay(7, HIGH);
+      break;      
+  }
+  
+  my_state++;
+  my_state %= NUM_STATES;
+}
+
+
+void button_ISR()
+{
+  if (digitalRead(button_pin) == LOW)
+  {
+    Serial.println("Button was pressed!");
+    my_func();
+  }
+  else
+  {
+    Serial.println("Button was released!");
+  }
+}
+
